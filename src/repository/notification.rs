b586abffc86186fc33a15ad5e157fb9cfcc2e629
Kallln <ahmad.haikal41@ui.ac.id>
2026-03-29 22:58:03 +0700

    Implement receive_notification function in Notification service.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -12,7 +12,7 @@ lazy_static! {
 pub struct NotificationRepository;
 
 impl NotificationRepository {
-    pub fn add(&self, notification: Notification) -> Notification {
+    pub fn add(notification: Notification) -> Notification {
         NOTIFICATIONS.write().unwrap()
             .push(notification.clone());
         return notification;
